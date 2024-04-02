d4aa979871f9c0fe2c52ca46563b26cf6f81f521
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-02 22:31:38 +0900

    :lipstick: 로그인 페이지 수정

--- dalim/pages/accounts/login.js
+++ dalim/pages/accounts/login.js
@@ -29,13 +29,33 @@ const Wrapper = styled.main`
 export default function Login() {
   const {login} = useContext(AuthContext);
 
+  const validateForm = (data)=>{
+    // 이메일 유효성 검사
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(data.email)) {
+      alert("유효한 이메일 주소를 입력해주세요.");
+      return false;
+    }
+  
+    // 비밀번호 유효성 검사
+    if (data.password.length < 8) {
+      alert("비밀번호는 최소 8자 이상이어야 합니다.");
+      return false;
+    }
+  
+    return true;
+  };
+
   const loginSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
     const data = new FormData(form);
+    const isValid = validateForm(Object.fromEntries(data));
     
-    // 유효성 검사 하고 login(data);
-    login(data);
+    // 유효성 검사를 통과한 경우, 로그인 로직을 수행
+    if (isValid) {
+      login(data);
+    }
   }
 
   return (
@@ -44,12 +64,12 @@ export default function Login() {
         <h2>로그인</h2>
         <ul>
           <li>
-            <label htmlFor='username'>아이디</label>
-            <input id="username" name="username" />
+            <label htmlFor='email'>이메일</label>
+            <input id="email" name="email" type="email" required/>
           </li>
           <li>
             <label htmlFor='password'>비밀번호</label>
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" required/>
           </li>
         </ul>
         <button className='default-btn'>로그인</button>
