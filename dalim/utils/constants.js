4cf8a9044cae3ade4af0df3594edefe9299c8c37
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-06 00:13:15 +0900

    :wrench: constants에 요일, 시간 선택지 추가

--- dalim/utils/constants.js
+++ dalim/utils/constants.js
@@ -8,3 +8,24 @@ export const location_city_list = [
     {"value":"jeju", "label":"제주"},
     {"value":"etc", "label":"기타"}
 ];
+
+export let meet_time_list = [];
+for (let hour = 0; hour < 24; hour++) {
+    for (let minute of [0, 30]) {
+        let period = hour < 12 ? "AM" : "PM";
+        let displayHour = hour % 12 || 12;
+        let displayMinute = minute.toString().padStart(2, '0');
+        let timeString = `${displayHour}:${displayMinute} ${period}`;
+        meet_time_list.push(timeString);
+    }
+}
+
+export const week_list = [
+    {"value":"mon", "label":"월"},
+    {"value":"tue", "label":"화"},
+    {"value":"wed", "label":"수"},
+    {"value":"thu", "label":"목"},
+    {"value":"fri", "label":"금"},
+    {"value":"sat", "label":"토"},
+    {"value":"sun", "label":"일"}
+];
