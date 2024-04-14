9bc513928bf6ebc43b55773d9e51d0831b39b913
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-14 17:31:09 +0900

    :link: 대회 상세 API 연결

--- dalim/pages/race/[race_id].js
+++ dalim/pages/race/[race_id].js
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/authContext";
 import styled from "styled-components";
 import ImgTitleSection from "../../components/ImgTitleSection";
-import { AuthContext } from "../../context/authContext";
 import Link from "next/link";
 
 
@@ -26,7 +26,6 @@ const Wrapper = styled.main`
         flex-direction: row-reverse;
         align-items: flex-start;
         position: relative;
-        /* flex-wrap: wrap; */
     }
 
     .race-info-area aside{
@@ -154,102 +153,178 @@ const Wrapper = styled.main`
 `;
 
 export default function RaceDetail(){
-    const {user} = useContext(AuthContext);
+    const {user, refreshToken} = useContext(AuthContext);
     const { race_id } = useRouter().query;
     const [race, setRace] = useState(null);
     const [reviews, setReviews] = useState([]);
     const [isFavorite, setIsFavorite] = useState(race?.is_favorite);
 
-    useEffect(() => {
-        // [TO DO] 서버에 요청을 보내서 race_id에 해당하는 대회 정보를 가져와야 함
-        // GET /races/<int:race_id>/
-        // 헤더 토큰 필요
-        const race_mock = {
-            "id": 1,
-            "title": "양천구마라톤",
-            "organizer":"양천구청",
-            "reg_status": "접수중",
-            "d_day":12,
-            "location": "서울 양천운동장",
-            "fees":10000,
-            "start_date": "2024/05/30",
-            "end_date": "2024/05/31",
-            "reg_start_date": "2024/04/15",
-            "reg_end_date": "2024/05/27",
-            "courses": ["Full", "Half", "10km"],
-            "description": "양천구청 주관 하프 마라톤 대회",
-            "thumbnail_image": "이미지 path",
-            "register_url": "대회 홈페이지 등록 링크",
-            "is_favorite": false,
-        };
-        const review_mock = [
-            {
-                "id": 1,
-                "author_id": 1,
-                "author_nickname": "닉네임",
-                "contents": "좋은 대회입니다!",
-                "created_at": "2023-06-01T10:00:00Z",
-                "updated_at": "2023-06-01T10:00:00Z"
-            },
-            {
-                "id": 2,
-                "author_id": 2,
-                "author_nickname": "닉네임",
-                "contents": "좋은 대회아상입니다!",
-                "created_at": "2024-06-01T10:00:00Z",
-                "updated_at": "2024-06-01T10:00:00Z"
+    const getRaceInfo = async () => {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/races/${race_id}`;
+        let headers = {};
+
+        if (user) {
+            headers["Authorization"] = `Bearer ${localStorage.getItem("dalim_access")}`;
+        }
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: headers,
+        });
+        
+        if (response.status === 404){
+            alert("해당하는 대회 정보가 없습니다.");
+            router.push("/404");
+        } else if (user && response.status === 401) {
+            console.log("토큰 재요청");
+            await refreshToken();
+            await getRaceInfo();
+        }
+
+        const data = await response.json();
+        setRace(data);
+        setIsFavorite(data.is_favorite);
+    };
+
+    const getReviews = async () => {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/races/${race_id}/reviews/`);
+        const data = await response.json();
+        setReviews(data);
+    };
+
+    const toggleFavorite = async () => {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/races/${race_id}/favorite/`;
+        let headers = {};
+
+        if (!user){
+            alert("로그인 후 이용해주세요.");
+            return;
+        } else {
+            headers["Authorization"] = `Bearer ${localStorage.getItem("dalim_access")}`;
+        }
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: headers,
+            data: {
+                user_id: user.pk,
             }
-        ];
-        // 대회 정보를 받아온 후에 race state에 저장
-        setRace(race_mock);
-        setReviews(review_mock);
-        // 해당하는 대회 정보 없는 경우 404 페이지로 이동
-    },[race_id])
+        });
 
+        if (response.status === 401){
+            console.log("토큰 재요청");
+            await refreshToken();
+            await toggleFavorite();
+        } else if (response.status === 204 || response.status === 201){
+            setIsFavorite(!isFavorite);
+        }
+    };
 
-    // API관련 함수
-    const patchReview = (review_id) => {
-        // 수정할 내용을 prompt로 입력받기
+    const patchReview = async(review_id) => {
         const mod_contents = prompt("수정할 덧글 내용을 입력해주세요");
 
         if (mod_contents){
-            // [TO DO] PATCH /races/<int:race_id>/reviews/<int:review_id>/
-            // 헤더 토큰 필요
-            // 응답 제대로 오면 setReviews
+            const url = `${process.env.NEXT_PUBLIC_API_URL}/races/${race_id}/reviews/${review_id}/`;
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("dalim_access")}`,                
+            };
+            const data = {
+                contents: mod_contents,
+            };
+            const response = await fetch(url, {
+                method: "PATCH",
+                headers: headers,
+                body: JSON.stringify(data),
+            });
+
+            if (response.status === 401){
+                console.log("토큰 재요청");
+                refreshToken();
+                patchReview(review_id);
+            } else if (response.status === 200){
+                getReviews();
+            } else {
+                alert("수정에 실패했습니다.");
+                console.log(response);
+            }
+
         }
     };
 
-    const deleteReview = (review_id) => {
-        // confirm으로 삭제 여부 확인
+    const deleteReview = async (review_id) => {
         const isDelete = confirm("정말로 삭제하시겠습니까?");
         
         if(isDelete){
-            // [TO DO] DELETE /races/<int:race_id>/reviews/<int:review_id>/
-            // 200? 204? 정상 응답 오면 setReviews
+            const url = `${process.env.NEXT_PUBLIC_API_URL}/races/${race_id}/reviews/${review_id}/`;
+            const headers = {
+                "Authorization": `Bearer ${localStorage.getItem("dalim_access")}`,
+            };
+            const response = await fetch(url, {
+                method: "DELETE",
+                headers: headers,
+            });
+
+            if (response.status === 401){
+                console.log("토큰 재요청");
+                refreshToken();
+                deleteReview(review_id);
+            } else if (response.status === 204){
+                getReviews();
+            } else {
+                alert("삭제에 실패했습니다.");
+                console.log(response);
+            }
         }
     };
 
-    const addReview = (e) => {
+    const addReview = async (e) => {
         e.preventDefault();
 
         const contents = e.target.querySelector("textarea").value;
         if (contents){
-            // [TO DO] POST /races/<int:race_id>/reviews/
-            // 헤더 토큰 필요
-            // 응답 제대로 오면 setReviews
+            const url = `${process.env.NEXT_PUBLIC_API_URL}/races/${race_id}/reviews/`;
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("dalim_access")}`,                
+            };
+            const data = {
+                contents: contents,
+            };
+            const response = await fetch(url, {
+                method: "POST",
+                headers: headers,
+                body: JSON.stringify(data),
+            });
+
+            if (response.status === 401){
+                console.log("토큰 재요청");
+                refreshToken();
+                addReview(e);
+            } else if (response.status === 201){
+                e.target.querySelector("textarea").value = "";
+                getReviews();
+            }
+
         } else {
             alert("내용을 입력해주세요.");
         }
     };
 
+    useEffect(() => {
+        if(race_id){
+            getRaceInfo();
+            getReviews();
+        }
+    },[race_id])
     
     return(
         <Wrapper>
             <ImgTitleSection
                 isFavorite={isFavorite}
-                setIsFavorite={setIsFavorite}
+                toggleFavorite={toggleFavorite}
                 name={race?.title}
-                badgeTxt={`${race?.d_day ? `D-${race?.d_day}` : ""} ${race?.reg_status}`}
+                badgeTxt={`${race?.reg_status}${race?.d_day>0 ? ` D${race?.d_day * -1}`: ""}`}
                 imgUrl={race?.thumbnail_image}
                 favApiPath={`/races/${race_id}/favorite/`}
             />
@@ -296,16 +371,18 @@ export default function RaceDetail(){
                                 className="default-btn"
                                 target="_blank"
                             >
-                                대회 가입하기
+                                대회 홈페이지 바로가기
                             </Link>
                         </p>
                     </div>
                 </aside>
                 <div className="race-description">
                     <p className="txt-b">대회 소개</p>
-                    <pre>
-                        {race?.description}
-                    </pre>
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html : race?.description
+                        }}
+                    />
                 </div>
             </section>
 
@@ -319,7 +396,7 @@ export default function RaceDetail(){
                                     <div className="user-area">
                                         <b>{review.author_nickname}님의 후기</b>
                                         {
-                                            review.author_id === user?.pk &&
+                                            review.author_id == user?.pk &&
                                             <p>
                                                 <button
                                                     onClick={()=>{patchReview(review.id);}}
@@ -346,7 +423,7 @@ export default function RaceDetail(){
                     user &&
                     <form className="default-form" onSubmit={addReview}>
                         <textarea placeholder="후기를 작성해주세요" maxLength={300}></textarea>
-                        <button className="default-btn small">질문 등록</button>
+                        <button className="default-btn small">후기 등록</button>
                     </form>
                 }
             </section>
