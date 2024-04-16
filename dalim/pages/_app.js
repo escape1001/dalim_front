f74aa5d03a34f51de3cb707e47db381317ed479c
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-16 17:43:06 +0900

    :wrench: meta 정보 추가

--- dalim/pages/_app.js
+++ dalim/pages/_app.js
@@ -14,6 +14,9 @@ export default function App({Component, pageProps}){
         <meta property="og:type" content="website"/>
         <meta name="twitter:card" content="summary"/>
         <link rel="shortcut icon" href={favicon.src} />
+        <meta property="og:title" content="달림(Dalim)  - 러너들의 커뮤니티"/>
+        <meta property="og:description" content="러닝 크루부터 대회, 러닝 일지 작성까지 한 곳에서! 함께 달리고, 함께 성장하는 러닝 플랫폼 '달림'"/>
+        {/* <meta property="og:image" content="https://ztajsmqfxrzwblwqdmil.supabase.co/storage/v1/object/public/yutorang/thumbnail_yutorang_osaka.png?t=2023-03-13T10%3A18%3A25.998Z"/> */}
       </Head>
       <AuthProvider>
         <Layout>
