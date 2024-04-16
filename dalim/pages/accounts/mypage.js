e649d74c1224dd2cf92dd12a276a77a4455fa352
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-04-16 17:42:54 +0900

    :lipstick: 프로필 이미지 없을 때 기본프사 보여주기

--- dalim/pages/accounts/mypage.js
+++ dalim/pages/accounts/mypage.js
@@ -446,7 +446,13 @@ export default function Mypage() {
         <h2 className='ir-hidden'>유저 기본정보</h2>
         <div className='info-area'>
           <p className='img-area'>
-            <img src={`${process.env.NEXT_PUBLIC_API_URL}${userInfo?.profile_image}`} alt=""/>
+            <img src={
+                userInfo?.profile_image ?
+                `${process.env.NEXT_PUBLIC_API_URL}${userInfo?.profile_image}` :
+                '/assets/images/default_profile.jpg'
+              }
+              alt=""
+            />
           </p>
           <div className='text-area'>
             <div className="greeting">
